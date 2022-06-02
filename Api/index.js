db0a20352f6cca76dc2637b5b5fd84207d34e899
MercadoST <santi_merca@live.com.ar>
2022-06-02 10:05:39 -0300

    Edits y Deletes

--- Api/index.js
+++ Api/index.js
@@ -3,6 +3,8 @@ const app = require('./app');
 async function main() {
     await app.listen(app.get('port'));
     console.log('Server port: ', app.get('port'))
+    let date = new Date();
+    console.log(date.toLocaleString());
 } 
 
 main();
--- Api/routes/index.js
+++ Api/routes/index.js
@@ -3,13 +3,22 @@ const router = Router();
 
 const fs = require('fs');
 const { send } = require('process');
+const { v4: uuidv4 } = require('uuid');
 const json_personas = fs.readFileSync('Api/db/asociado.json', 'utf-8');
+const json_pedidos = fs.readFileSync('Api/db/pedidos.json', 'utf-8');
+const json_solicitantes = fs.readFileSync('Api/db/solicitantes.json', 'utf-8');
+const json_donaciones= fs.readFileSync('Api/db/donaciones.json', 'utf-8');
 let personas = JSON.parse(json_personas);
+let pedidos = JSON.parse(json_pedidos);
+let solicitantes = JSON.parse(json_solicitantes);
+let donaciones = JSON.parse(json_donaciones);
+
 
 router.get('/', (req, res) =>{
     res.render('Index.ejs')
 })
-
+//asociados 
+//#region
 router.get('/AgAsociado', (req, res) =>{
     res.render('AgAsociado.ejs')
 })
@@ -26,17 +35,27 @@ router.post('/AgAsoc', (req, res) => {
     var ingasoc = JSON.stringify(personas);
     fs.writeFileSync('Api/db/asociado.json', ingasoc, 'utf-8' );
     console.log('hecho');
-    res.redirect('/');
+    res.redirect('/Asociados');
     // aca agregar la funcion para que agregre en el JSON
 })
 
-router.get('/EdAsoc', (req, res) => {
-    res.send('Funco');
+router.get('/EdAsoc/:dni', (req, res) => {
+    var edit = personas.find(asoc => asoc.dni == req.params.dni)
+    res.render('EditAsociado.ejs', {
+        edit
+    })
     // aca agregar la funcion para que busque en el JSON
 })
 
 router.post('/EdAsoc', (req, res) => {
-    res.send('Funco post');
+    personas = personas.filter(asoc => asoc.dni != req.body.dni)
+    var asoc = req.body;
+    personas.push(asoc);
+    var ingasoc = JSON.stringify(personas);
+    fs.writeFileSync('Api/db/asociado.json', ingasoc, 'utf-8' );
+    console.log('hecho');
+
+    res.redirect('/Asociados');
     // aca agregar la funcion para que edite en el JSON
 })
 
@@ -45,7 +64,145 @@ router.get('/BorrAsoc/:dni', (req, res) => {
     let ingasoc = JSON.stringify(personas);
     fs.writeFileSync('Api/db/asociado.json', ingasoc, 'utf-8' );
     res.redirect('/Asociados');
-    // aca agregar la funcion para que elimine en el JSON (post o delete?)
+    // aca agregar la funcion para que elimine en el JSON 
+})
+ //#endregion
+
+//soilicitantes
+//#region
+router.get('/AgSolicitante', (req, res) =>{
+    res.render('AgSolicitante.ejs')
 })
 
+router.get('/Solicitantes', (req, res) =>{
+    res.render('Solicitantes.ejs', {
+        solicitantes
+    })
+})
+
+router.post('/AgSoli', (req, res) => {
+    var soli = req.body;
+    solicitantes.push(soli);
+    var ingsoli = JSON.stringify(solicitantes);
+    fs.writeFileSync('Api/db/solicitantes.json', ingsoli, 'utf-8' );
+    console.log('hecho');
+    res.redirect('/Solicitantes');
+    // aca agregar la funcion para que agregre en el JSON
+})
+
+router.get('/EdSoli/:cuit', (req, res) => {
+    var edit = solicitantes.find(soli => soli.cuit == req.params.cuit)
+    res.render('EditSolicitante.ejs', {
+        edit
+    })
+    // aca agregar la funcion para que busque en el JSON
+})
+
+router.post('/EdSoli', (req, res) => {
+    solicitantes = solicitantes.filter(soli => soli.cuit != req.body.cuit)
+    var soli = req.body;
+    solicitantes.push(soli);
+    var ingsoli = JSON.stringify(solicitantes);
+    fs.writeFileSync('Api/db/solicitantes.json', ingsoli, 'utf-8' );
+    console.log('hecho');
+
+    res.redirect('/Solicitantes');
+    // aca agregar la funcion para que edite en el JSON
+})
+
+router.get('/BorrSoli/:cuit', (req, res) => {
+    solicitantes = solicitantes.filter(soli => soli.cuit != req.params.cuit)
+    let ingsoli = JSON.stringify(solicitantes);
+    fs.writeFileSync('Api/db/solicitantes.json', ingsoli, 'utf-8' );
+    res.redirect('/Solicitantes');
+    // aca agregar la funcion para que elimine en el JSON 
+})
+ //#endregion
+
+ //pedidos 
+//#region
+router.get('/AgPedido', (req, res) =>{
+    res.render('AgPedido.ejs', {
+        solicitantes
+    })
+})
+
+router.get('/Pedidos', (req, res) =>{
+    res.render('Pedidos.ejs', {
+        pedidos
+    })
+})
+
+router.post('/AgPed', (req, res) => {
+    let date = new Date();
+    console.log(date.toLocaleDateString());
+    const {solicitante, grsan, cantidad, fvencimiento } = req.body;
+    let pedi =
+    {
+        id: uuidv4(),
+        solicitante,
+        grsan,
+        cantidad,
+        femision: date.toLocaleDateString() ,
+        fvencimiento,
+        estado: "No Completado"
+    }
+
+    pedidos.push(pedi);
+    var ingpedi = JSON.stringify(pedidos);
+    fs.writeFileSync('Api/db/pedidos.json', ingpedi, 'utf-8' );
+    console.log('hecho');
+    res.redirect('/Pedidos');
+    // aca agregar la funcion para que agregre en el JSON
+})
+
+router.get('/ComPed/:id', (req, res) => {
+    var pedi = pedidos.find(pedi => pedi.id == req.params.id)
+    pedidos = pedidos.filter(pedi => pedi.id != req.params.id);
+    pedi.estado = "Completado";
+    pedidos.push(pedi);
+    var ingpedi = JSON.stringify(pedidos);
+    fs.writeFileSync('Api/db/pedidos.json', ingpedi, 'utf-8' );
+    console.log('hecho');
+    res.redirect('/Pedidos');
+    // aca agregar la funcion para que agregre en el JSON
+})
+
+router.get('/EdPed/:id', (req, res) => {
+    var edit = pedidos.find(pedi => pedi.id == req.params.id)
+    res.render('EditAsociado.ejs', {
+        edit,
+        solicitantes
+    })
+    // aca agregar la funcion para que busque en el JSON
+})
+
+router.post('/EdPed', (req, res) => {
+    var edit = pedidos.find(pedi => pedi.id == req.body.id)
+    pedidos = pedidos.filter(pedi => pedi.id != req.body.id)
+    edit.solicitante= req.body.solicitante;
+    edit.fvencimiento= req.body.fvencimiento;
+    edit.grsan = req.body.grsan;
+    edit.cantidad = req.body.cantidad;
+    pedidos.push(edit);
+    var ingpedi = JSON.stringify(pedidos);
+    fs.writeFileSync('Api/db/pedidos.json', ingpedi, 'utf-8' );
+    console.log('hecho');
+
+    res.redirect('/Pedidos');
+    // aca agregar la funcion para que edite en el JSON
+})
+
+router.get('/BorrPed/:id', (req, res) => {
+    pedidos = pedidos.filter(pedi => pedi.id != req.params.id)
+    let ingpedi = JSON.stringify(pedidos);
+    fs.writeFileSync('Api/db/pedidos.json', ingpedi, 'utf-8' );
+    res.redirect('/Pedidos');
+    // aca agregar la funcion para que elimine en el JSON 
+})
+ //#endregion
+
+// donaciones
+
+ 
 module.exports = router;
